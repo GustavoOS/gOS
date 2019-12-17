7595f7ff625b39a604e82bd07176f7b80f6e435c
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2019-12-17 03:58:20 -0300

    Feature/bios (#1)
    * Create first BIOS code
    
    * New BIOS compilation
    
    * New Bios

---
+++ src/bios.c
@@ -0,0 +1,45 @@
+int one;
+int zero;
+int hdStart;
+int osAddress;
+
+void setupVariables(void)
+{
+    zero = 0;
+    one = 1;
+    osAddress = 2048;
+    hdStart = 16384;
+}
+
+void moveData(int srcAddr, int targetAddr)
+{
+    int data;
+    data = readFromMemory(srcAddr);
+    writeIntoMemory(targetAddr, data);
+}
+
+void insertOSIntoMemory(void)
+{
+    int osLength;
+    int index;
+    int srcAddr;
+    int targetAddr;
+
+    osLength = readFromMemory(hdStart);
+    index = zero;
+    srcAddr = hdStart + one;
+    targetAddr = osAddress;
+    while (index < osLength)
+    {
+        moveData(srcAddr + index, targetAddr + index);
+        index = index + one;
+    }
+    output(readFromMemory(2060));
+}
+
+int main(void)
+{
+    setupVariables();
+    insertOSIntoMemory();
+    return 0;
+}
