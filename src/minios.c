28ef952d41244996be0ba69758860f814d658bf6
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-02-01 23:24:08 -0300

    Optimize mini OS

--- src/minios.c
+++ src/minios.c
@@ -1,46 +1,19 @@
 int registers;
 int null;
 
-// void printRegisters(void)
-// {
-//     output(readFromMemory(6135));
-//     output(readFromMemory(6136));
-//     output(readFromMemory(6137));
-//     output(readFromMemory(6138));
-//     output(readFromMemory(6139));
-//     output(readFromMemory(6140));
-//     output(readFromMemory(6141));
-//     output(readFromMemory(6142));
-// }
-
-// void printStack(void)
-// {
-//     int sp;
-
-//     sp = readFromMemory(6137);
-//     while (sp < 8192)
-//     {
-//         output(sp);
-//         output(readFromMemory(sp));
-//         sp = sp + 1;
-//     }
-// }
-
 void saveStack(void)
 {
     int fileIndex;
     int stackIndex;
     int sp;
-    int item;
 
     fileIndex = 19859;
-    stackIndex = 8191;
-    sp = readFromMemory(6137);
+    stackIndex = 8192;
+    sp = readFromMemory(registers + 3);
 
-    while (sp < (stackIndex + 1))
+    while (sp < stackIndex)
     {
-        item = readFromMemory(stackIndex);
-        writeIntoMemory(fileIndex, item);
+        writeIntoMemory(fileIndex, readFromMemory(stackIndex - 1));
         stackIndex = stackIndex - 1;
         fileIndex = fileIndex - 1;
     }
@@ -51,16 +24,14 @@ void loadStack(void)
     int fileIndex;
     int stackIndex;
     int sp;
-    int item;
 
     fileIndex = 19859;
-    stackIndex = 8191;
-    sp = readFromMemory(6137);
+    stackIndex = 8192;
+    sp = readFromMemory(registers + 3);
 
-    while (sp < (stackIndex + 1))
+    while (sp < stackIndex)
     {
-        item = readFromMemory(fileIndex);
-        writeIntoMemory(stackIndex, item);
+        writeIntoMemory(stackIndex - 1, readFromMemory(fileIndex));
         stackIndex = stackIndex - 1;
         fileIndex = fileIndex - 1;
     }
@@ -70,41 +41,34 @@ void insertProgramIntoMemory(void)
 {
     int instructionCount;
     int copied;
-    int fullWords;
     int slotStart;
     int data;
 
     slotStart = 18444;
     instructionCount = readFromMemory(slotStart);
-    slotStart = slotStart + 1; // where program start
+    slotStart = slotStart + 1; // where program starts
     copied = 0;
-    fullWords = 0;
     while (copied < instructionCount)
     {
-        data = readFromMemory(slotStart + fullWords);
+        data = readFromMemory(slotStart + (copied / 2));
         writeIntoMemory(copied, extractFirstHW(data));
         writeIntoMemory(copied + 1, extractSecondHW(data));
-        fullWords = fullWords + 1;
         copied = copied + 2;
     }
 }
 
-void continueProcess(void)
+void continueProcess(int slotEnd)
 {
-    int stackpointer;
-    int stackIndex;
-    stackpointer = readFromMemory(19873);
-
     insertProgramIntoMemory();
-    // Save registers
-    writeIntoMemory(registers + 8, readFromMemory(19868)); // Acumulator
-    writeIntoMemory(registers + 7, readFromMemory(19869)); // Temporary Register
-    writeIntoMemory(registers + 6, readFromMemory(19870)); // Frame Pointer
-    writeIntoMemory(registers + 5, readFromMemory(19871)); // Global Pointer
-    writeIntoMemory(registers + 4, readFromMemory(19872)); // Return Address
-    writeIntoMemory(registers + 3, stackpointer);          // SP
-    writeIntoMemory(registers + 2, readFromMemory(19874)); // PC
-    writeIntoMemory(registers + 1, readFromMemory(19875)); // SpecReg
+    // Load registers
+    writeIntoMemory(registers + 8, readFromMemory(slotEnd - 7)); // Acumulator
+    writeIntoMemory(registers + 7, readFromMemory(slotEnd - 6)); // Temporary Register
+    writeIntoMemory(registers + 6, readFromMemory(slotEnd - 5)); // Frame Pointer
+    writeIntoMemory(registers + 5, readFromMemory(slotEnd - 4)); // Global Pointer
+    writeIntoMemory(registers + 4, readFromMemory(slotEnd - 3)); // Return Address
+    writeIntoMemory(registers + 3, readFromMemory(slotEnd - 2)); // SP
+    writeIntoMemory(registers + 2, readFromMemory(slotEnd - 1)); // PC
+    writeIntoMemory(registers + 1, readFromMemory(slotEnd));     // SpecReg
 
     loadStack();
 }
@@ -121,30 +85,27 @@ void runNewProcess(void)
 
 void takeUserAction(void)
 {
-    int userInput;
     int menu;
     menu = 0;
     while (menu == menu)
     {
         if (menu == 0)
         {
-            output(49374);
-            userInput = input();
-            if (userInput != 0)
+            output(49374); // C0DE
+            if (input() != 0)
             {
-                output(789996);
+                output(789996); // C0DEC
                 runNewProcess();
                 return;
             }
         }
         if (menu == 1)
         {
-            output(51966);
-            userInput = input();
-            if (userInput != 0)
+            output(51966); // CAFE
+            if (input() != 0)
             {
-                output(831468);
-                continueProcess();
+                output(831468); // CAFEC
+                continueProcess(19875);
                 return;
             }
         }
@@ -157,23 +118,22 @@ void takeUserAction(void)
 
 int ioFlow(void)
 {
-    writeIntoMemory(registers + 2, readFromMemory(registers + 2) + 1); // PC++ to avoid loop
+    writeIntoMemory(registers + 2,
+                    readFromMemory(registers + 2) + 1); // PC++
     return 0;
 }
 
 void saveState(void)
 {
-    int codeSize;
     int stackpointer;
-    int savedStackItemAddr;
+    int minSP;
     stackpointer = readFromMemory(registers + 3);
-    codeSize = readFromMemory(18444);
-    codeSize = (codeSize + 1) / 2;
+    minSP = readFromMemory(18444) + 1;
+    minSP = (minSP / 2) + 6777;
 
-    if (stackpointer < 6776 + codeSize)
+    if (stackpointer < minSP)
         return;
-    // printRegisters();
-    // Save Registers
+
     writeIntoMemory(19868, readFromMemory(registers + 8)); // Acumulator
     writeIntoMemory(19869, readFromMemory(registers + 7)); // Temporary Register
     writeIntoMemory(19870, readFromMemory(registers + 6)); // Frame Pointer
@@ -183,15 +143,6 @@ void saveState(void)
     writeIntoMemory(19874, readFromMemory(registers + 2)); // PC
     writeIntoMemory(19875, readFromMemory(registers + 1)); // SpecReg
 
-    // printStack();
-    // // Save Stack
-    // savedStackItemAddr = 18445 + codeSize;
-    // while (stackpointer < 8191)
-    // {
-    //     writeIntoMemory(savedStackItemAddr, readFromMemory(stackpointer));
-    //     stackpointer = stackpointer + 1;
-    //     savedStackItemAddr = savedStackItemAddr + 1;
-    // }
     saveStack();
 }
 
