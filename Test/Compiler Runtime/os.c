9d8b95f640ed578a44d9245f1ff990d559fe9de4
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-11-28 15:28:53 -0300

    Fix test loop

--- Test/Compiler Runtime/os.c
+++ Test/Compiler Runtime/os.c
@@ -5,10 +5,11 @@ int main(void)
     int i;
     i = 0;
     assignPointer(context, 6134);
-    while (i < 9)
+    while (i < 10)
     {
         output(context[i]);
         i = i + 1;
     }
+    context[2] = 3;
     return 0;
 }
