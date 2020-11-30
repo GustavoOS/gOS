2a7fa9a37ed60b6355c2f9676369819032273f38
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-11-30 18:55:26 -0300

    Simplify OS to check Variable comeback

--- Test/Compiler Runtime/os.c
+++ Test/Compiler Runtime/os.c
@@ -2,15 +2,14 @@ int context[0];
 
 int main(void)
 {
-    int i;
-    i = 0;
+    // int i;
+    // i = 0;
     assignPointer(context, 6133);
-    while (i < 10)
-    {
-        output(context[i]);
-        i = i + 1;
-    }
+    // while (i < 10)
+    // {
+    //     output(context[i]);
+    //     i = i + 1;
+    // }
     context[2] = 3;
-    output(49568); //C1A0
     return 0;
 }
