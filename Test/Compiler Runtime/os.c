c2a926fd72add1ee7ac7f59903d0319ae4248ce8
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-12-01 00:06:28 -0300

    Runtime Full Test Pass

--- Test/Compiler Runtime/os.c
+++ Test/Compiler Runtime/os.c
@@ -2,14 +2,14 @@ int context[0];
 
 int main(void)
 {
-    // int i;
-    // i = 0;
+    int i;
+    i = 0;
     assignPointer(context, 6133);
-    // while (i < 10)
-    // {
-    //     output(context[i]);
-    //     i = i + 1;
-    // }
+    while (i < 10)
+    {
+        output(context[i]);
+        i = i + 1;
+    }
     context[2] = 3;
     return 0;
 }
