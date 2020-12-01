84594f9cff1bd24ff7cf21cd831a3594070b6849
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-12-01 00:14:02 -0300

    Feature/make heap accessible by os (#6)
    * Create test to verify the runtime context content
    
    * Create test mif files
    
    * Fix test loop
    
    * Fix Stack position of values
    
    * Improve runtime test UX
    
    * Fix index of array
    
    * Simplify OS to check Variable comeback
    
    * Runtime Full Test Pass

---
+++ Test/Compiler Runtime/os.c
@@ -0,0 +1,15 @@
+int context[0];
+
+int main(void)
+{
+    int i;
+    i = 0;
+    assignPointer(context, 6133);
+    while (i < 10)
+    {
+        output(context[i]);
+        i = i + 1;
+    }
+    context[2] = 3;
+    return 0;
+}
