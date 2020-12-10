1e14393166ea882bbf8b30ae7e742fbc0e5dad65
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-12-10 13:44:19 -0300

    Optimize BIOS

--- src/bios.c
+++ src/bios.c
@@ -1,23 +1,21 @@
 int main(void)
 {
     int osLength;
-    int index;
-    int data;
     int osStorage[0];
     int osRuntime[0];
 
     assignPointer(osRuntime, 3072);
     assignPointer(osStorage, 8193);
     osLength = readFromMemory(8192);
-    index = 0;
-    while (index < osLength)
+    while (osLength > 0)
     {
-        data = osStorage[index / 2];
-        osRuntime[index] = extractFirstHW(data);
-        osRuntime[index + 1] = extractSecondHW(data);
-        index = index + 2;
+        osRuntime[0] = extractFirstHW(osStorage[0]);
+        osRuntime[1] = extractSecondHW(osStorage[0]);
+        assignPointer(osRuntime, readPointer(osRuntime) + 2);
+        assignPointer(osStorage, readPointer(osStorage) + 1);
+        osLength = osLength - 2;
     }
-    output(osRuntime[osLength - 1]);
+    output(readFromMemory(readFromMemory(8192) + 3071)); // Last OS instruction
     output(49568); // C1A0
     return 0;
 }
