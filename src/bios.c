f50fd1017785cc4024ae69688c66349284caeaba
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2020-12-10 13:50:10 -0300

    rename bios variable to remaining instructions

--- src/bios.c
+++ src/bios.c
@@ -1,21 +1,21 @@
 int main(void)
 {
-    int osLength;
+    int remainingInstructions;
     int osStorage[0];
     int osRuntime[0];
 
     assignPointer(osRuntime, 3072);
     assignPointer(osStorage, 8193);
-    osLength = readFromMemory(8192);
-    while (osLength > 0)
+    remainingInstructions = readFromMemory(8192); // Size of OS
+    while (remainingInstructions > 0)
     {
         osRuntime[0] = extractFirstHW(osStorage[0]);
         osRuntime[1] = extractSecondHW(osStorage[0]);
         assignPointer(osRuntime, readPointer(osRuntime) + 2);
         assignPointer(osStorage, readPointer(osStorage) + 1);
-        osLength = osLength - 2;
+        remainingInstructions = remainingInstructions - 2;
     }
     output(readFromMemory(readFromMemory(8192) + 3071)); // Last OS instruction
-    output(49568); // C1A0
+    output(49568);                                       // C1A0
     return 0;
 }
